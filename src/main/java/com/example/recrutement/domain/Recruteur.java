a7aaa7f78424bf386c3a7f054aed504af24fbffb
KHELIFI Ahmed Aziz <ahmedazizkhelifi@gmail.com>
2022-04-23 10:53:45 +0100

    recruter fix

--- src/main/java/com/example/recrutement/domain/Recruteur.java
+++ src/main/java/com/example/recrutement/domain/Recruteur.java
@@ -15,6 +15,69 @@ public class Recruteur extends user {
     @Column(name="telephone")
     private long tel;
 
+    @Column(name="adress    ")
+    private String adress;
+
+
+    @Column(name="description")
+    private String description;
+
+
+    @Column(name="gov")
+    private String  gov;
+
+
+    @Column(name="sector")
+    private String   sector;
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public Recruteur(Long id, String username, String email, String password, String nom_prenom, long tel, String adress, String description, String gov, String sector) {
+        super(id, username, email, password, nom_prenom);
+        this.tel = tel;
+        this.adress = adress;
+        this.description = description;
+        this.gov = gov;
+        this.sector = sector;
+    }
+
+    public Recruteur(long tel, String adress, String description, String gov, String sector) {
+        this.tel = tel;
+        this.adress = adress;
+        this.description = description;
+        this.gov = gov;
+        this.sector = sector;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGov() {
+        return gov;
+    }
+
+    public void setGov(String gov) {
+        this.gov = gov;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
 
     public Recruteur(long tel) {
         this.tel = tel;
